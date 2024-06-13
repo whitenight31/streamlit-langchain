24a544aa07911905c4ebd323cf01b2669d53b524
송석리(Song Sukree) <greatsong21@gmail.com>
2024-06-14 08:50:06 +0900

    Update 01-나만의ChatGPT.py
    from langchain_community.chat_models import ChatOpenAI

--- pages/01-나만의ChatGPT.py
+++ pages/01-나만의ChatGPT.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
